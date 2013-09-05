48a874ab176daf03a17cdf4c6aaeaf8c3dc7180d
gelicia <kdurivage@gelicia.com>
2013-09-04 22:13:56 -0500

    dont turn off LEDs in wheel, get scrollimage and scroll and swap image to work

--- main/skirt2013/skirt2013.ino
+++ main/skirt2013/skirt2013.ino
@@ -12,14 +12,47 @@ int party[] PROGMEM = {
   0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
 };
 
+int spaceInvaders[] PROGMEM = {
+  0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,  0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0,  0, 0, 0, 0, 1, 1, 0, 0, 0, 
+  0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,  0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0,  0, 0, 0, 1, 1, 1, 1, 0, 0, 
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0,  0, 0, 1, 1, 1, 1, 1, 1, 0, 
+  0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1,  0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0,  0, 1, 1, 0, 1, 1, 0, 1, 1, 
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  0, 1, 1, 1, 1, 1, 1, 1, 1, 
+  0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0,  0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1,  0, 0, 0, 1, 0, 0, 1, 0, 0, 
+  0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0,  0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1,  0, 0, 1, 0, 1, 1, 0, 1, 0, 
+  0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,  0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0,  0, 1, 0, 1, 0, 0, 1, 0, 1, 
+};
+
+int spaceInvaders2[] PROGMEM = {
+  0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,  0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0,  0, 0, 0, 0, 1, 1, 0, 0, 0, 
+  0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,  0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1,  0, 0, 0, 1, 1, 1, 1, 0, 0, 
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1,  0, 0, 1, 1, 1, 1, 1, 1, 0, 
+  0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1,  0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1,  0, 1, 1, 0, 1, 1, 0, 1, 1, 
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  0, 1, 1, 1, 1, 1, 1, 1, 1, 
+  0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0,  0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,  0, 0, 0, 1, 0, 0, 1, 0, 0, 
+  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,  0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0,  0, 0, 1, 0, 1, 1, 0, 1, 0, 
+  0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0,  0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0,  0, 1, 0, 0, 0, 0, 0, 0, 1,
+};
 
 struct Image {
-int width, height, *pixels;
+  int width, height, *pixels;
+  int palette[50];
 };
 
 struct Image party_image = {
-  43, 8, party
+  43, 8, party, {128}
 };
+
+struct Image spaceInv_image = {
+  34, 8,
+  spaceInvaders
+};
+
+struct Image spaceInv2_image = {
+  34, 8,
+  spaceInvaders2
+};
+
 
 // Number of RGB LEDs in strand:
 int LEDsW = 10;
@@ -39,29 +72,31 @@ void setup() {
 }
 
 void loop() {
-  scrollImage(&party_image, 0);
-//  setLED(5,5,128);
-//  strip.setPixelColor(39, strip.Color(0,   0, 127));
- // strip.show();
+  scrollImage(&party_image, 60);
+  //setLED(0,0,128);
+  //strip.setPixelColor(39, strip.Color(0,   0, 127));
+  //strip.show();
+  
+  //scrollAndAlternateImage(&spaceInv_image, &spaceInv2_image, 4, 90);
 }
 
+//todo fix to be dynamic
 void scrollImage(struct Image *image, int del){
   int width = image->width;
   const int *pixels = image->pixels;
-  strip.show();
+  //strip.show();
   //iterate through each column of the image at an offset
   for(int i=0; i<width; i++){
     //update the display
-    for(int y=0; y<width; y++){
-      for(int x=0; x<width; x++){
-        int what = pgm_read_byte(&pixels[y*width + (x + i)%width]);
-        if (what == 1){
-         setLED(x, y, 128); 
+    for(int y=0; y<LEDsH; y++){
+      for(int x=0; x<LEDsW; x++){
+        int colorIdx = pgm_read_byte(&pixels[y*width + (x + i)%width]);
+        int color = 0;
+        if (colorIdx != 0){
+          color = image->palette[colorIdx-1];
         }
-        else {
-          setLED(x, y, 0); 
-        }
-        Serial.println(what);
+        setLED(x, y, color); 
+        
       } 
     }
     strip.show();
@@ -69,51 +104,76 @@ void scrollImage(struct Image *image, int del){
   }
 }
 
+//Alternate between two images while scrolling
+void scrollAndAlternateImage(struct Image *image1, struct Image *image2, int altDelay, int del){
+  int width = image1->width;
+  
+  //iterate through each column of the image at an offset
+  for(int i=0; i<width; i++){
+    const int *pixels ;
+    if (i&altDelay) {
+      pixels = image1->pixels;
+    }
+    else {
+     pixels = image2->pixels;
+    }
+    
+    //update the display
+    for(int y=0; y<8; y++){
+      for(int x=0; x<10; x++){
+        int what = pgm_read_byte(&pixels[y*width + (x + i)%width]);
+        if (what == 1){
+          setLED(x, y, 128); 
+        }
+        else {
+          setLED(x, y, 0); 
+        }
+      } 
+    }
+    strip.show();
+    delay(del);
+  }
+}
+
+
+//sets an LED of x,y 
+//color is 0-385, with 0 being off
 void setLED(uint32_t x, uint32_t y, uint32_t color){
-  int ledAddr = (8 * (9-x)) + (x&1==0?(7-y):y);
-  strip.setPixelColor(ledAddr, Wheel(color));
+  int ledAddr = (8 * (9-x)) + (((x&1)==0)?(7-y):(y));
+  strip.setPixelColor(ledAddr, color==0?color:Wheel(color-1));
 }
 
 void clearLEDs(){
   for(int y=0; y<LEDsH; y++){
-      for(int x=0; x<LEDsW; x++){
-        setLED(x,y,0);
-      }
+    for(int x=0; x<LEDsW; x++){
+      setLED(x,y,0);
+    }
   }
 }
 
-//changed so 0 is off
-//colors are 1 to 385
+//Input a value 0 to 384 to get a color value.
+//The colours are a transition r - g -b - back to r
+
 uint32_t Wheel(uint16_t WheelPos)
 {
   byte r, g, b;
-  
-  if(WheelPos == 0){
-    return 0;
+  switch(WheelPos / 128)
+  {
+    case 0:
+      r = 127 - WheelPos % 128;   //Red down
+      g = WheelPos % 128;      // Green up
+      b = 0;                  //blue off
+      break; 
+    case 1:
+      g = 127 - WheelPos % 128;  //green down
+      b = WheelPos % 128;      //blue up
+      r = 0;                  //red off
+      break; 
+    case 2:
+      b = 127 - WheelPos % 128;  //blue down 
+      r = WheelPos % 128;      //red up
+      g = 0;                  //green off
+      break; 
   }
-  else {
-    //set WheelPos to what it was and use original algorithm
-    WheelPos = WheelPos - 1;
-    
-    switch(WheelPos / 128)
-    {
-      case 0:
-        r = 127 - WheelPos % 128;   //Red down
-        g = WheelPos % 128;      // Green up
-        b = 0;                  //blue off
-        break; 
-      case 1:
-        g = 127 - WheelPos % 128;  //green down
-        b = WheelPos % 128;      //blue up
-        r = 0;                  //red off
-        break; 
-      case 2:
-        b = 127 - WheelPos % 128;  //blue down 
-        r = WheelPos % 128;      //red up
-        g = 0;                  //green off
-        break; 
-    }
-    return(strip.Color(r,g,b));
-  
-  }
+  return(strip.Color(r,g,b));
 }
