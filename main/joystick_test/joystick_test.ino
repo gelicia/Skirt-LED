9c6e237c92be3b1591a98bae1dc49d7df9d256ec
gelicia <kdurivage@gelicia.com>
2013-10-26 14:39:27 -0500

    adding joystick testing and first draft at tetris

---
+++ main/joystick_test/joystick_test.ino
@@ -0,0 +1,43 @@
+int xSensorPin = A1;
+int ySensorPin = A0;
+int selectButtonPin = 13;
+int xSensorValue = 0;
+int ySensorValue = 0;
+//int selectButtonValue;
+String outString = "";
+
+void setup() {
+  Serial.begin(9600); 
+  pinMode(selectButtonPin, INPUT);
+}
+
+void loop() {
+ // outString = "";
+  // read the value from the sensor:
+   //xSensorValue = analogRead(xSensorPin);    
+   //ySensorValue = analogRead(ySensorPin); 
+   int selectButtonValue = digitalRead(selectButtonPin);
+   
+   /*if (xSensorValue < 20){
+     outString = outString + "LEFT";
+   }
+   else if(xSensorValue > 1000){
+     outString = outString + "RIGHT";
+   }
+   
+   if (ySensorValue < 20){
+     outString = outString + "DOWN";
+   }
+   else if(ySensorValue > 1000){
+     outString = outString + "UP";
+   }
+   
+   outString = outString + selectButtonValue;
+   
+  // String commaString = ", ";  
+  //String serialString = xSensorValue + commaString + ySensorValue;*/
+  Serial.println(selectButtonValue);
+
+  delay(50);
+   
+}
---
+++ main/tetris/tetris/tetris.ino
@@ -0,0 +1,66 @@
+
+struct Pos {
+  int x, y;
+}
+
+struct Piece {
+  int rotationCount;
+  Pos **rotations;
+}
+
+struct Pos oPiece_rotations [][4] = {
+  {{0,0}, {0,-1}, {1,0}, {1,-1}}
+}
+
+struct Pos lPiece_rotations [][4] = {
+  {{0,0}, {1,0}, {-1,0}, {-1,1}},
+  {{0,0}, {0,1}, {0,-1}, {-1,-1}},
+  {{0,0}, {-1,0}, {1,0}, {1,-1}},
+  {{0,0}, {0,-1}, {0,1}, {1,1}},
+}
+
+struct Pos jPiece_rotations [][4] = {
+  {{0,0}, {-1,0}, {1,0}, {1,1}},
+  {{0,0}, {0,-1}, {0,1}, {-1,1}},
+  {{0,0}, {1,0}, {-1,0}, {-1,-1}},
+  {{0,0}, {0,1}, {0,-1}, {1,-1}}
+}
+
+struct Pos iPiece_rotations [][4] = {
+  {{0,0}, {-1,0}, {1,0}, {2,0}},
+  {{0,0}, {0,1}, {0,-1}, {0,-2}}
+}
+
+struct Pos sPiece_rotations [][4] = {
+  {{0,0}, {1,0}, {0,1}, {-1,1}},
+  {{0,0}, {0,1}, {1,1}, {1,2}}
+}
+
+struct Pos zPiece_rotations [][4] = {
+  {{0,0}, {-1,0}, {0,1}, {1,1}},
+  {{0,0}, {0,-1}, {-1,0}, {-1,1}}
+}
+
+struct Pos tPiece_rotations [][4] = {
+  {{0,0}, {-1,0}, {0,1}, {1,1}},
+  {{0,0}, {0,-1}, {-1,0}, {-1,1}}
+}
+
+
+//0, 0 is never included
+struct Pos sqPiece[] = {
+{1,0}, {1,1}, {0,1}
+}
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
